064f058678b94c2d5fb71da7c1b179b64b5e8bcd
Vermavnish <166979960+Vermavnish@users.noreply.github.com>
2025-05-25 14:39:55 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
